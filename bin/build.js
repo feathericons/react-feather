e4d871fe4d283c47c048291e5b115351bc9bc9d1
Mackie Underdown <macklinu@users.noreply.github.com>
2018-07-01 10:38:44 -0700

    build: generate type definitions
    fixes #13

--- bin/build.js
+++ bin/build.js
@@ -8,12 +8,29 @@ const prettier = require('prettier');
 
 const rootDir = path.join(__dirname, '..');
 
+const initialTypeDefinitions = `/// <reference types="react" />
+import { ComponentType, SVGAttributes } from 'react';
+
+interface Props extends SVGAttributes<SVGElement> {
+  color?: string;
+  size?: string | number;
+}
+
+type Icon = ComponentType<Props>;
+`;
+
 glob(`${rootDir}/src/feather/icons/**.svg`, (err, icons) => {
   fs.writeFileSync(path.join(rootDir, 'src', 'index.js'), '', 'utf-8');
+  fs.writeFileSync(
+    path.join(rootDir, 'src', 'index.d.ts'),
+    initialTypeDefinitions,
+    'utf-8'
+  );
 
-  icons.forEach((i) => {
+  icons.forEach(i => {
     const svg = fs.readFileSync(i, 'utf-8');
     const id = path.basename(i, '.svg');
+    const ComponentName = uppercamelcase(id);
     const $ = cheerio.load(svg, {
       xmlMode: true,
     });
@@ -21,9 +38,11 @@ glob(`${rootDir}/src/feather/icons/**.svg`, (err, icons) => {
     const location = path.join(rootDir, 'src/icons', fileName);
 
     $('*').each((index, el) => {
-      Object.keys(el.attribs).forEach((x) => {
+      Object.keys(el.attribs).forEach(x => {
         if (x.includes('-')) {
-          $(el).attr(camelcase(x), el.attribs[x]).removeAttr(x);
+          $(el)
+            .attr(camelcase(x), el.attribs[x])
+            .removeAttr(x);
         }
         if (x === 'stroke') {
           $(el).attr(x, 'currentColor');
@@ -39,20 +58,19 @@ glob(`${rootDir}/src/feather/icons/**.svg`, (err, icons) => {
       import React from 'react';
       import PropTypes from 'prop-types';
 
-      const ${uppercamelcase(id)} = (props) => {
+      const ${ComponentName} = (props) => {
         const { color, size, ...otherProps } = props;
         return (
-          ${
-            $('svg').toString()
-              .replace(new RegExp('stroke="currentColor"', 'g'), 'stroke={color}')
-              .replace('width="24"', 'width={size}')
-              .replace('height="24"', 'height={size}')
-              .replace('otherProps="..."', '{...otherProps}')
-          }
+          ${$('svg')
+            .toString()
+            .replace(new RegExp('stroke="currentColor"', 'g'), 'stroke={color}')
+            .replace('width="24"', 'width={size}')
+            .replace('height="24"', 'height={size}')
+            .replace('otherProps="..."', '{...otherProps}')}
         )
       };
 
-      ${uppercamelcase(id)}.propTypes = {
+      ${ComponentName}.propTypes = {
         color: PropTypes.string,
         size: PropTypes.oneOfType([
           PropTypes.string,
@@ -60,12 +78,12 @@ glob(`${rootDir}/src/feather/icons/**.svg`, (err, icons) => {
         ]),
       }
 
-      ${uppercamelcase(id)}.defaultProps = {
+      ${ComponentName}.defaultProps = {
         color: 'currentColor',
         size: '24',
       }
 
-      export default ${uppercamelcase(id)}
+      export default ${ComponentName}
     `;
 
     const component = prettier.format(element, {
@@ -77,7 +95,18 @@ glob(`${rootDir}/src/feather/icons/**.svg`, (err, icons) => {
 
     fs.writeFileSync(location, component, 'utf-8');
 
-    const exportString = `export ${uppercamelcase(id)} from './icons/${id}';\r\n`;
-    fs.appendFileSync(path.join(rootDir, 'src', 'index.js'), exportString, 'utf-8');
+    const exportString = `export ${ComponentName} from './icons/${id}';\r\n`;
+    fs.appendFileSync(
+      path.join(rootDir, 'src', 'index.js'),
+      exportString,
+      'utf-8'
+    );
+
+    const exportTypeString = `export const ${ComponentName}: Icon;\n`;
+    fs.appendFileSync(
+      path.join(rootDir, 'src', 'index.d.ts'),
+      exportTypeString,
+      'utf-8'
+    );
   });
 });
